5565bbc553c705d2adb50beaeff69355de86b987
Ruslan Konev <rkonev@ozon.ru>
2019-05-22 12:18:19 +0300

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: 'universal',
+  server: {
+    port: 8000, // default: 3000
+    // host: '0.0.0.0', // default: localhost
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'ant-design-vue/dist/antd.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/antd-ui'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Mastering Web3 with Waves
+      </h1>
+      <div class="links">
+        <a
+          href="https://bitcoinnews.blog"
+          target="_blank"
+          rel="noopener"
+          class="button--grey"
+        >Read Web3</a>
+        <a
+          href="https://wavesplatform.com"
+          target="_blank"
+          rel="noopener"
+          class="button--grey"
+        >Connect with Waves</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #0a82ff;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
